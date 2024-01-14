761d8d81341ff9a849014d48e101ddb37b8478ea
nyutiz <gabgenheu@gmail.com>
2024-01-14 11:49:40 +0100

    Added code

---
+++ src/main/java/com/nyutiz/TrainingData.java
@@ -0,0 +1,13 @@
+package com.nyutiz;
+
+public class TrainingData {
+ 
+    float[] data;
+    float[] expectedOutput;
+   
+    public TrainingData(float[] data, float[] expectedOutput) {
+        this.data = data;
+        this.expectedOutput = expectedOutput;
+    }
+   
+}
